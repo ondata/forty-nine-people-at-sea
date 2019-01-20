5012d38fe0a1a55b8c5260f730cbef05395747da
aborruso <aborruso@gmail.com>
2019-01-20 23:25:04 +0100

    aggiornato agli eventi del 19 gennaio 2019

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
+<!-- TOC -->
+
+- [Versione corrente (2019-01-20)](#versione-corrente-2019-01-20)
+    - [Un tweet periodico per seguire la nave "SEA WATCH 3"](#un-tweet-periodico-per-seguire-la-nave-sea-watch-3)
+- [Vecchia versione](#vecchia-versione)
+    - [Un tweet periodico per seguire le navi "SEA WATCH 3" e "PROF ALBRECHT PENCK", a partire dai dati](#un-tweet-periodico-per-seguire-le-navi-sea-watch-3-e-prof-albrecht-penck-a-partire-dai-dati)
+
+<!-- /TOC -->
+# Versione corrente (2019-01-20)
+
+## Un tweet periodico per seguire la nave "SEA WATCH 3"
+
+Il 19 gennaio 2019 l'equipaggio della "SEA WATCH 3" ha salvto 47 persone dal mare.
+
+Alle 8, alle 13, alle 18 e alle 22 verrà pubblicato un tweet come quello di [sotto](https://twitter.com/ondatait/status/1082909680681259008). È generato automaticamente a partire dalla **raccolta** di alcuni **dati** (al momento della pubblicazione del tweet):
+
+- quanti **giorni** sono **passati** a partire dal 19 gennaio 2019;
+- le **coordinate** della nave (grazie a https://www.vesselfinder.com) e a partire da queste;
+  - **temperatura** e **altezza d'onda** (grazie a https://stormglass.io);
+  - un'**immagine** con la **mappa** con la posizione della nave (grazie a [Mapbox](https://www.mapbox.com/api-documentation/#static) e [OpenStreetMap](https://www.openstreetmap.org/))
+
+[![](./imgs/tweet.png)](https://twitter.com/ondatait/status/1082909680681259008)
+
+
+# Vecchia versione
+
 ## Un tweet periodico per seguire le navi "SEA WATCH 3" e "PROF ALBRECHT PENCK", a partire dai dati
 
 Alle 8, alle 13, alle 18 e alle 22 verrà pubblicato un tweet come quello di [sotto](https://twitter.com/ondatait/status/1082909680681259008). È generato automaticamente a partire dalla **raccolta** di alcuni **dati** (al momento della pubblicazione del tweet):
--- ve.sh
+++ ve.sh
@@ -4,8 +4,8 @@ set -x
 
 folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 time=$(date +%s)
-giorni=$((($(date +%s)-$(date +%s --date "2018-12-22"))/(3600*24)))
-persone="49"
+giorni=$((($(date +%s)-$(date +%s --date "2019-01-19"))/(3600*24)))
+persone="47"
 ora=$(date +"%k")
 oraW=$(echo "$ora*100" | bc)
 
@@ -36,7 +36,7 @@ meteoSW=$(curl -sL "https://api.stormglass.io/point?lat=$latSW&lng=$lonSW&params
 waveHeightSW=$(echo "$meteoSW" | tr "\n" "," | cut -d ',' -f 2)
 airTemperatureSW=$(echo "$meteoSW" | tr "\n" "," | cut -d ',' -f 1)
 
-
+<<comment1
 ## PROF ALBRECHT PENCK Coordinates
 curl -sL -c "$folder"/cookie 'https://www.vesselfinder.com/it/vessels/PROF-ALBRECHT-PENCK-IMO-5285667-MMSI-211215130' -H 'authority: www.vesselfinder.com' -H 'cache-control: max-age=0' -H 'upgrade-insecure-requests: 1' -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.17 Safari/537.36' -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'referer: https://www.vesselfinder.com/it/?imo=5285667' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: it,en-US;q=0.9,en;q=0.8'  --compressed  >/dev/null
 
@@ -56,18 +56,20 @@ meteoPAP=$(curl -sL "https://api.stormglass.io/point?lat=$latPAP&lng=$lonPAP&par
 
 waveHeightPAP=$(echo "$meteoSW" | tr "\n" "," | cut -d ',' -f 2)
 airTemperaturePAP=$(echo "$meteoSW" | tr "\n" "," | cut -d ',' -f 1)
+comment1
 
 ## map image
-coordsY=$(echo "($latSW+$latPAP)/2.0" | bc -l)
-coordsX=$(echo "($lonSW+$lonPAP)/2.0" | bc -l)
-wget -O "$folder"/map.png "https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/pin-s-1+9ed4bd($lonSW,$latSW),pin-s-2+000($lonPAP,$latPAP)/$coordsX,$coordsY,8,0,0/1024x512?access_token=$mapboxAPI"
+#coordsY=$(echo "($latSW+$latPAP)/2.0" | bc -l)
+#coordsX=$(echo "($lonSW+$lonPAP)/2.0" | bc -l)
+coordsY="$latSW"
+coordsX="$lonSW"
+# wget -O "$folder"/map.png "https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/pin-s-1+9ed4bd($lonSW,$latSW),pin-s-2+000($lonPAP,$latPAP)/$coordsX,$coordsY,8,0,0/1024x512?access_token=$mapboxAPI"
+wget -O "$folder"/map.png "https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/pin-s-1+9ed4bd($lonSW,$latSW)/$coordsX,$coordsY,5,0,0/1024x512?access_token=$mapboxAPI"
 
 ## tweet
 testo='
-Da '"$giorni"' giorni '"$persone"' persone costrette a bordo di due navi:
-1) #SEAWATCH 3, temperatura '"$airTemperatureSW"' °C, onda '"$waveHeightSW"' m '"$urlSW"'
-2) PROF ALBRECHT PENCK, temperatura '"$airTemperaturePAP"' °C, onda '"$waveHeightPAP"' m '"$urlPAP"'
-@GiuseppeConteIT @DaniloToninelli #ApriteIPorti
+Da '"$giorni"' giorni '"$persone"' sono a bordo di #SEAWATCH 3: temperatura di '"$airTemperatureSW"' °C, onda di '"$waveHeightSW"' m '"$urlSW"'
+#ApriteIPorti
 '
 
 twurl set default ondatait
